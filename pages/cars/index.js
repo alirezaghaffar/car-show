ab70e1bee12158977ec8b9382dd869aff42477d3
alirezaGH <alireza.ghaffar.js@gmail.com>
2023-07-03 13:46:29 +0330

    create CarsPage component and its module get data from prop

---
+++ pages/cars/index.js
@@ -0,0 +1,11 @@
+import carsData from "../../data/carsdata";
+import CarsPage from "../../components/templates/CarsPage";
+const Details = () => {
+    return (
+        <div>
+               <CarsPage  data={carsData}/>
+        </div>
+    );
+};
+
+export default Details;
