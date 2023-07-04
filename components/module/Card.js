8b738d43e3ed9874e0a05a7d9e6cdcc97554189d
alirezaGH <alireza.ghaffar.js@gmail.com>
2023-07-04 16:52:36 +0330

    create cart create details page

--- components/module/Card.js
+++ components/module/Card.js
@@ -1,23 +1,26 @@
+import Link from "next/link";
 import styles from "./Card.module.css";
 
-import Location from "../icons/Location"
+import Location from "../icons/Location";
 
 const Card = (props) => {
-    const { id, name, model, year, location, image, price ,distance } = props
+    const { id, name, model, year, location, image, price, distance } = props
     return (
-        <div className={styles.container}>
-            <img src={image} className={styles.image} />
-            <h4 className={styles.title}>{`${name} ${model}`}</h4>
-            <p className={styles.detail}>{`${year} . ${distance}`}</p>
-            <div className={styles.footer}>
-                <p>${price}</p>
-                <div className={styles.location}>
-                    <p>{location}</p>
-                    <Location />
+        <Link href={`/cars/${id}`}>
+            <div className={styles.container}>
+                <img src={image} className={styles.image} />
+                <h4 className={styles.title}>{`${name} ${model}`}</h4>
+                <p className={styles.detail}>{`${year} . ${distance}`}</p>
+                <div className={styles.footer}>
+                    <p>${price}</p>
+                    <div className={styles.location}>
+                        <p>{location}</p>
+                        <Location />
+                    </div>
                 </div>
             </div>
-        </div>
+        </Link>
     );
 };
 
-export default Card;
+export default Card; 
---
+++ pages/cars/[carid].js
@@ -0,0 +1,16 @@
+import { useRouter } from "next/router";
+import carsData from "../../data/carsdata";
+import CarDetails from "../../components/templates/CarsDetails"
+
+const CarDetail = () => {
+    const router = useRouter();
+    const {carid} = router.query;
+    const carDetails = carsData[carid-1]
+    return (
+
+        <CarDetails {...carDetails} />
+
+    );
+};
+
+export default CarDetail;
